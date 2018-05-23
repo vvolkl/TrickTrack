7c2232f0cc7353d4a14d057abeeebdd01f89810f
Valentin Volkl <valentin.volkl@cern.ch>
2018-05-23 14:00:11 +0200

    up

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ git apply --stat python2.7.patch
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install
 make install -j 4
+cd ..
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/install/lib/:$PWD/install/lib64
+export PYTHONPATH=$PYTHONPATH:$PWD/install/lib/:$PWD/install/lib64
+
 
 
